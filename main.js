f489fd5cffb88807f65a098ac2fbbecbcd616b12
joexavier-1997 <joexavier1997@gmail.com>
2019-09-12 23:49:49 +0530

    added files

---
+++ main.js
@@ -0,0 +1,46 @@
+function list() {
+    console.log()
+    var x = document.getElementById("input").value;
+    var t = document.createTextNode(x);
+    var cr = document.createElement("li");
+    cr.setAttribute("id",input.value)
+    cr.appendChild(t);
+    document.getElementById("todolist").appendChild(cr);    
+}
+
+const todo_template = `
+<div id='div_id'>
+    <div>
+        <input id="input">
+        <button onclick="list()">add</button>
+        <button onclick="remove_list()">remove</button>
+    </div>
+    <ol id="todolist">
+
+    </ol>
+</div>`
+
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+  
+    // Change this to div.childNodes to support multiple top-level nodes
+    return div.firstChild; 
+  }
+
+function remove_list() {
+    var r=document.getElementById("todolist");
+    // console.log(r);   
+    var x = document.getElementById(input.value);
+    // console.log(x);
+    r.removeChild(x);
+}
+function newlist() {
+    todo_list_element = createElementFromHTML(todo_template)
+    // console.log
+    // var div = document.getElementById('div_id'),
+    // clone = div.cloneNode(true);
+    // console.log(clone);
+    // clone.id = 'some_id';
+    document.body.appendChild(todo_list_element);
+}

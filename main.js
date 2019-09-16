800188caa49b673df5c50b81dc5f2e646bebf9e6
joexavier-1997 <joexavier1997@gmail.com>
2019-09-16 17:59:55 +0530

    basic

--- main.js
+++ main.js
@@ -1,44 +1,62 @@
-function list() {
-    console.log()
-    var x = document.getElementById("input").value;
-    var t = document.createTextNode(x);
-    var cr = document.createElement("li");
-    cr.setAttribute("id",input.value)
-    cr.appendChild(t);
-    document.getElementById("todolist").appendChild(cr);    
+let a=1;
+const TODO_POSTFIX = "_todolist";
+const INPUT_POSTFIX = "_input";
+
+function create() {
+    
+    const todoId = a;
+
+
+    // let div = createOuterDiv(todoId);
+    // let inputBox = createInput(div);
+    // let addBtn = createButton(div, getAddItemFn(todoId));
+    // let removeBtn = createButton(div);
+
+    let div1=document.createElement('div');
+    div1.setAttribute("id",a.toString());
+    document.body.appendChild(div1);
+    a++;
+    let inp = document.createElement('input')
+    div1.appendChild(inp);
+    inp.setAttribute("id", todoId + INPUT_POSTFIX);
+    let b1=document.createElement('button');
+    let b2=document.createElement('button');
+    let todoList = document.createElement("ol");
+    todoList.setAttribute("id", todoId + TODO_POSTFIX);
+    div1.appendChild(b1);
+    div1.appendChild(b2);
+    div1.appendChild(todoList);
+    
+    b1.innerHTML="add";
+    b2.innerHTML="remove";
+    b1.addEventListener("click", getAddItemFn(todoId));
+    b1.setAttribute("id",todoId + INPUT_POSTFIX)
+    b2.addEventListener("click", getRemoveItemFn(todoId));
+}
+function enter(input){
+    input.addEventListener("keyup",function(event){
+    if(event.keyCode===13){
+        event.preventDefault();
+        document.getElementById(parentId + INPUT_POSTFIX).click();
+    }
+})
 }
 
-const todo_template = `
-<div id='div_id'>
-    <div>
-        <input id="input">
-        <button onclick="list()">add</button>
-        <button onclick="remove_list()">remove</button>
-    </div>
-    <ol id="todolist">
-
-    </ol>
-</div>`
-
-function createElementFromHTML(htmlString) {
-    var div = document.createElement('div');
-    div.innerHTML = htmlString.trim();
-      return div.firstChild; 
-  }
-
-function remove_list() {
-    var r=document.getElementById("todolist");
-    // console.log(r);   
-    var x = document.getElementById(input.value);
-    // console.log(x);
-    r.removeChild(x);
+function getAddItemFn(parentId) {
+    return function(e) {
+        var x = document.getElementById(parentId + INPUT_POSTFIX);
+       
+        var t = document.createTextNode(x.value);
+        var cr = document.createElement("li");
+        cr.setAttribute("id",x)
+        cr.appendChild(t);
+        document.getElementById(parentId + TODO_POSTFIX).appendChild(cr);    
+    }
 }
-function newlist() {
-    todo_list_element = createElementFromHTML(todo_template)
-    // console.log
-    // var div = document.getElementById('div_id'),
-    // clone = div.cloneNode(true);
-    // console.log(clone);
-    // clone.id = 'some_id';
-    document.body.appendChild(todo_list_element);
+
+function getRemoveItemFn(parentId){
+    return function(e) {
+        let r = document.getElementById(parentId + TODO_POSTFIX);
+        r.removeChild(r.childNodes[0])
+    }
 }

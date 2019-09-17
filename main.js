42e3ad2a946becba2813f63d878997761e30de2b
joexavier-1997 <joexavier1997@gmail.com>
2019-09-17 14:31:38 +0530

    final

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const TODO_POSTFIX = "_todolist";
 const INPUT_POSTFIX = "_input";
 const BUTTON_POSTFIX = "_button";
 function create() {
-    
     const todoId = a;
     let div1=document.createElement('div');
     div1.setAttribute("id",a.toString());
@@ -13,15 +12,15 @@ function create() {
     div1.appendChild(inp);
     inp.setAttribute("id", todoId + INPUT_POSTFIX);
     let b1=document.createElement('button');
-    let b2=document.createElement('button');
+    //let b2=document.createElement('button');
     let todoList = document.createElement("ol");
     todoList.setAttribute("id", todoId + TODO_POSTFIX);
     div1.appendChild(b1);
-    div1.appendChild(b2);
+    //div1.appendChild(b2);
     div1.appendChild(todoList);
     
     b1.innerHTML="add";
-    b2.innerHTML="remove";
+    //b2.innerHTML="remove";
     let addFn = getAddItemFn(todoId);
     b1.addEventListener("click", addFn)
     inp.addEventListener("keydown",function(e){
@@ -30,9 +29,14 @@ function create() {
         }
     });
     
- 
     b1.setAttribute("id",todoId + BUTTON_POSTFIX)
-    b2.addEventListener("click", getRemoveItemFn(todoId));
+    todoList.addEventListener("click", function (e){
+        var tgt = e.target;
+        console.log(tgt);
+        if (tgt.tagName == "LI") {
+          tgt.parentNode.removeChild(tgt); 
+        }
+      });
 }
 
 function getAddItemFn(parentId) {
@@ -44,19 +48,19 @@ function getAddItemFn(parentId) {
              cr.setAttribute("id",x)
              cr.appendChild(t);
              document.getElementById(parentId + TODO_POSTFIX).appendChild(cr);
-             console.log(x); 
              x.value="";
         }
     }
 }
 
-function getRemoveItemFn(parentId){
-    return function(e) {
-        let r = document.getElementById(parentId + TODO_POSTFIX);
-        r.removeChild(r.childNodes[0])
-    }
-}
+// function getRemoveItemFn(parentId){
+//     return function(e) {
+//         let r = document.getElementById(parentId + TODO_POSTFIX);
+//         let x = document.getElementById(parentId + INPUT_POSTFIX);
+//         if(r.hasChildNodes()) 
+//         x.value="";
+//     }
 
-
+// }
 
 create();
